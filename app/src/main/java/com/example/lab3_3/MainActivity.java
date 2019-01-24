b324bd91b6e2fb937a17772fddd1420748d14ef8
Samuli <samuli.ronkko@gmail.com>
2019-01-24 13:33:32 +0200

    Valmis

--- app/src/main/java/com/example/lab3_3/MainActivity.java
+++ app/src/main/java/com/example/lab3_3/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.zip.Inflater;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     MediaPlayer mediaPlayer;
+    private int elainLuokka;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.second_image_id).setOnClickListener(this);
         findViewById(R.id.third_image_id).setOnClickListener(this);
         findViewById(R.id.fourth_image_id).setOnClickListener(this);
+
+        elainLuokka = 0;
 
     }
 
@@ -46,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ((ImageView)findViewById(R.id.second_image_id)).setImageResource(R.drawable.peippo);
             ((ImageView)findViewById(R.id.third_image_id)).setImageResource(R.drawable.peukaloinen);
             ((ImageView)findViewById(R.id.fourth_image_id)).setImageResource(R.drawable.punatulkku);
+            elainLuokka = 1;
 
         }
         else if (item.getItemId() == R.id.mammals_item) {
@@ -53,6 +57,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ((ImageView)findViewById(R.id.second_image_id)).setImageResource(R.drawable.wolf);
             ((ImageView)findViewById(R.id.third_image_id)).setImageResource(R.drawable.elephant);
             ((ImageView)findViewById(R.id.fourth_image_id)).setImageResource(R.drawable.lamb);
+            elainLuokka = 0;
+
         }
 
         return true;
@@ -60,25 +66,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.first_image_id) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.bear);
-            mediaPlayer.start();
-        }
-        else if (v.getId() == R.id.second_image_id) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.wolf);
-            mediaPlayer.start();
-        }
-        else if (v.getId() == R.id.third_image_id) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.elephant);
-            mediaPlayer.start();
-        }
-        else if (v.getId() == R.id.fourth_image_id) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.lamb);
-            mediaPlayer.start();
-        }
 
+        if (elainLuokka == 0) {
 
-
-
+            if (v.getId() == R.id.first_image_id) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.bear);
+                mediaPlayer.start();
+            } else if (v.getId() == R.id.second_image_id) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.wolf);
+                mediaPlayer.start();
+            } else if (v.getId() == R.id.third_image_id) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.elephant);
+                mediaPlayer.start();
+            } else if (v.getId() == R.id.fourth_image_id) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.lamb);
+                mediaPlayer.start();
+            }
+        }
+        else if (elainLuokka == 1) {
+            if (v.getId() == R.id.first_image_id) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.huuhkaja_norther_eagle_owl);
+                mediaPlayer.start();
+            } else if (v.getId() == R.id.second_image_id) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.peippo_chaffinch);
+                mediaPlayer.start();
+            } else if (v.getId() == R.id.third_image_id) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.peukaloinen_wren);
+                mediaPlayer.start();
+            } else if (v.getId() == R.id.fourth_image_id) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.punatulkku_northern_bullfinch);
+                mediaPlayer.start();
+            }
+        }
     }
 }
